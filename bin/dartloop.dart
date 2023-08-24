dd821941ba6491d060467b92a301090d3e7b57c3
M R Computer <hossaintuhin6666@gmail.com>
2023-08-24 11:31:52 +0600

    loopcontinue

---
+++ bin/dartloop.dart
@@ -0,0 +1,5 @@
+void main() {
+  for (var m = 1; m < 51; m++) {
+    print("${m} dart");
+  }
+}
---
+++ bin/loopcontinue.dart
@@ -0,0 +1,9 @@
+void main() {
+  for (var i = 0; i < 10; i++) {
+    if (i == 4) {
+      continue;
+    }
+
+    print(i);
+  }
+}
